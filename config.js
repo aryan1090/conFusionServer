e5897916a1d2354f3fd648b6f24b235bbf3e35dc
aryan1090 <aryanchowdhary2172@gmail.com>
2021-01-22 19:44:25 +0530

    Removed Facebook Token

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
 module.exports = {
     'secretKey':"12345-67890-09876-54321",
     'mongoUrl':'mongodb://localhost:27017/conFusion',
-    'facebook':{
-        clientId: '1892303900912903',
-        clientSecret: '67a547be9ad809e96edef5fff35faf81'
-    }
+    
 }
